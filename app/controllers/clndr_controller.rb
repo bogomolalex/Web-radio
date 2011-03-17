62fe4e094a99ceb5a8ccfa255d68e66fc989307d
unknown <bogomol@.investbank.local>
2011-03-17 10:37:18 +0200

    get_clndr - created

--- app/controllers/clndr_controller.rb
+++ app/controllers/clndr_controller.rb
@@ -1,2 +1,11 @@
 class ClndrController < ApplicationController
+
+
+  def show
+   @vd=params[:vd]
+   @end_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i+1)-1
+   @begin_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i)
+   @arr_cl=get_clndr(@begin_of_date,@end_of_date)
+  end
+
 end
--- test/functional/clndr_controller_test.rb
+++ test/functional/clndr_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class ClndrControllerTest < ActionController::TestCase
+
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+   @xdat1=Date.today-30
+   @xdat2=Date.today
+  end
+  
+  def test_get_array_of_days
+   @arr = get_clndr(@xdat1,@xdat2)
+   assert_not_nil @arr,"Массив календаря пуст"
+   assert_not_equal 0,@arr.size,"Массив имеет размер 0"
   end
 end
