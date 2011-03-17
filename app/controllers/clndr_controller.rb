6a319c7991897a6a43b816d0ff14837b21922c2c
unknown <bogomol@.investbank.local>
2011-03-17 09:30:43 +0200

    creating calendar

--- app/controllers/clndr_controller.rb
+++ app/controllers/clndr_controller.rb
@@ -1,9 +1,2 @@
 class ClndrController < ApplicationController
-
-  def show
-   @vd=params[:vd]
-   @end_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i+1)-1
-   @begin_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i)
-  end
-
 end
