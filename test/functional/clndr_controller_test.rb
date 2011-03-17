74584b521c788b46b03fadab809bd189fb065a15
unknown <bogomol@.investbank.local>
2011-03-17 14:41:45 +0200

    test of calndar fixed

--- test/functional/clndr_controller_test.rb
+++ test/functional/clndr_controller_test.rb
@@ -4,12 +4,11 @@ class ClndrControllerTest < ActionController::TestCase
 
   # Replace this with your real tests.
   def setup
-   @xdat1=Date.today-30
-   @xdat2=Date.today
+   @xdat=Date.today
   end
   
   def test_get_array_of_days
-   @arr = get_clndr(@xdat1,@xdat2)
+   @arr = get_clndr(@xdat)
    assert_not_nil @arr,"Массив календаря пуст"
    assert_not_equal 0,@arr.size,"Массив имеет размер 0"
   end
