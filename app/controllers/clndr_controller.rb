ebe29ac7dfff35e064d11dd385efb64c651f2dcf
unknown <bogomol@.investbank.local>
2011-03-17 14:38:20 +0200

    view of calendar changed

--- app/controllers/clndr_controller.rb
+++ app/controllers/clndr_controller.rb
@@ -2,9 +2,8 @@ class ClndrController < ApplicationController
 
   def show
    @vd=t(params[:vd])
-   @end_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i+1)-1
-   @begin_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i)
-   @arr_cl=get_clndr(@begin_of_date,@end_of_date)
+   @begin_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i,@vd[-10,2].to_i)
+   @arr_cl=get_clndr(@begin_of_date)
   end
 
 
