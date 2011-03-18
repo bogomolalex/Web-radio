564db5f7da028ce6b7b9a8cee212c1ae0647c046
Alexander Bogomol <some@email.ru>
2011-03-18 10:41:03 +0200

    rebuilded view of calendar

--- app/controllers/clndr_controller.rb
+++ app/controllers/clndr_controller.rb
@@ -3,8 +3,5 @@ class ClndrController < ApplicationController
   def show
    @vd=t(params[:vd])
    @begin_of_date=Date.new(@vd[-4,4].to_i,@vd[-7,2].to_i,@vd[-10,2].to_i)
-   @arr_cl=get_clndr(@begin_of_date)
   end
-
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,28 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+ #
+ # Массив календаря
+ #
+  def get_clndr(xdat)
+   begin_of_date=Date.new(xdat.year,xdat.month,1)
+   end_of_date=Date.new(xdat.year,xdat.month+1,1)-1
+   # 
+   ar=(begin_of_date..end_of_date).map{|dn| [dn.cwday,dn.day,dn.strftime('%d-%m-%Y')] }
+   ar_beg=(1..ar[0][0]-1).map do |xday| 
+     [xday,0]
+   end
+   ar_end=(ar[ar.size-1][0]+1..7).map do |xday| 
+     [xday,0]
+   end
+   #
+   ar_beg+ar+ar_end
+  end
+
+  def show_calendar(xdate,key={})
+   @arr_cl=get_clndr(xdate)
+   @a_hash=key
+   @xdate=xdate
+   render :partial => 'clndr/clndr'
+  end
 end
