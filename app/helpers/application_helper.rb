157b8f7239423b304d6650e4262730cda3ca2305
Alexander Bogomol <some@email.ru>
2011-03-22 16:52:32 +0200

    added CRUD buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,8 +26,11 @@ module ApplicationHelper
 
   def show_table(xpar)
    xpar[:fields]||=["id"]
+   xpar[:crud]||=true
+   xpar[:url]||=""
    render :partial => 'shared/tab_vw',:locals=>{:headers=>xpar[:headers],
-                :objs=>xpar[:objs],:fields=>xpar[:fields]}
+                :objs=>xpar[:objs],:fields=>xpar[:fields],:crud=>xpar[:crud],
+                :url=>xpar[:url]}
   end
 
 end
