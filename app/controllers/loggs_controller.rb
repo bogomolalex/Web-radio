f0980ddb5dabf1489fe26bb1e1aedb4525d50ec4
Alexander Bogomol <some@email.ru>
2011-09-22 16:12:26 +0300

    Logg controller was restored

--- app/controllers/loggs_controller.rb
+++ app/controllers/loggs_controller.rb
@@ -1,2 +1,10 @@
 class LoggsController < ApplicationController
+  before_filter :login_required
+  layout 'msys'
+
+  def show
+    @logs = Loggs.paginate(:all,:per_page=>sysparam('per_page'),
+                           :page=>params[:page]||'1',:order=>"created_at desc")
+  end
+
 end
