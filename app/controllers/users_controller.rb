5582b3f1df5d425032e7a54739aa7ff23a43b791
Alexander Bogomol <some@email.ru>
2011-10-03 10:56:19 +0300

    ff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,20 @@ class UsersController < ApplicationController
     logout_keeping_session!
     @user = User.new(params[:user])
     success = @user && @user.save
+
     if success && @user.errors.empty?
             # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
       self.current_user = @user # !! now logged in
+      l=Loggs.new
+      l.username=self.current_user
+      l.message="Создан новый пользователь. IP: #{request.remote_ip}"
+      l.event='CREATED'
+      l.created_at=Time.now
+      l.logg_date=Date.today
+      l.save
       redirect_back_or_default('/')
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
